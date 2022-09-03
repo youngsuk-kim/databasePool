b6427d34bf5d5343f68f33f2733ef7f722cf4af6
youngsuk-kim <thayer2392@gmail.com>
2022-09-04 05:22:32 +0900

    initial commit

---
+++ src/main/kotlin/com/example/laboratory/LaboratoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.laboratory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaboratoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<LaboratoryApplication>(*args)
+}
